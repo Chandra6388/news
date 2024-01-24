aaece24c8d7bdd8b236bdfd0fbf17755029e109f
Chandra Prakash <cppatel6388@gmail.com>
2024-01-24 09:28:34 +0530

    first commit

---
+++ src/app/Store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from '../features/Counter/CounterSlice'
+
+export const store = configureStore({
+    reducer: counterReducer,
+})
+
+ 
+
+
---
+++ src/features/News/newsSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    value: ''
+}
+
+export const newsSlice = createSlice({
+    name:'news',
+    initialState,
+    reducers: {
+        
+    }
+})
